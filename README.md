a7b8d500a2e913e772be34bd5ec4387cb954b251
Matheus Cauã Polletti <matheus.c.polletti@gmail.com>
2024-09-04 10:13:13 -0300

    Atualização

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Calculadora-CSharp
- Criação de uma calculadora em C# para treinar meu conhecimento em C# e Programação Orientada a Objetos(POO)
+Criação de uma calculadora em C# para treinar meu conhecimento em C# e Programação Orientada a Objetos(POO).
+Muito obrigado por ter vindo.
--- obj/Debug/net8.0/Calculadora.AssemblyInfo.cs
+++ obj/Debug/net8.0/Calculadora.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Calculadora")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2febd81008356d3ade4f80bcc26e589127d6058f")]
 [assembly: System.Reflection.AssemblyProductAttribute("Calculadora")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Calculadora")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
