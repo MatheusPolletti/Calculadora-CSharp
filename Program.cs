7ab3e1f0460de15982e6b5fbfc01d62069684c09
Matheus Cauã Polletti <matheus.c.polletti@gmail.com>
2024-09-04 23:04:18 -0300

    AtualizaçãoFinal

--- Program.cs
+++ Program.cs
@@ -3,50 +3,52 @@ Console.OutputEncoding = Encoding.UTF8;
 
 Console.Title = "Calculadora";
 
-static bool Verificador(string num_1, string num_2, string item_operacao)
+static bool Parar()
 {
-    if (listaVerificar.Count() > 3 || listaVerificar.Count() < 3)
+    try
     {
-        Console.WriteLine("ERRO! O número de elementos não está correto para uma operação!");
-        return false;
-    }
-    else
-    {
-        try
+        Console.WriteLine("Você deseja parar? [S/N]");
+        string? resposta = Console.ReadLine()!.ToUpper();
+
+        if (resposta == "S")
         {
-            int num1 = Convert.ToInt32(num_1);
-            int num2 = Convert.ToInt32(num_2);
-            char valorOperacao = Convert.ToChar(item_operacao);
-            if ("+-/*".Contains(valorOperacao))
-            {
-                return true;
-            }
+            return true;
         }
-        catch
+        else if (resposta == "N")
         {
-            Console.WriteLine("ERRO! O primeiro elemento digitado não é um número inteiro legível!");
             return false;
         }
+        else
+        {
+            return Parar();
+        }
     }
-    return false;
+    catch
+    {
+        Console.WriteLine("Algo deu errado.");
+        return false;
+    }
 }
 
-Console.WriteLine("Digite a operação que você deseja realizar: ( + - / * )");
-string? operacao = Console.ReadLine();
+int continuar = 1;
 
-string[] partes_operacao = operacao.Split();
-string num1 = partes_operacao[0]
-bool valores_testados = Verificador(partes_operacao);
-
-if (valores_testados == true)
+while (continuar != 0)
 {
-    Calculadora calculadora = new Calculadora(, valor2, operacaoAtual!);
+    Console.WriteLine("Digite a operação que você deseja realizar: ( + - / * )");
+    string? operacao = Console.ReadLine();
+
+    string[] itens_operacao = operacao.Split();
+
+    Calculadora calculadora = new Calculadora(itens_operacao);
+    calculadora.show();
+
+    bool continuar_parar = Parar();
+
+    if (continuar_parar)
+    {
+        break;
+    }
 }
-
-//calculadora.calcular();
-//calculadora.show();
-
-Console.ReadKey();
 
 class Calculadora
 {
@@ -54,13 +56,98 @@ class Calculadora
     private int valorB;
     private char operacao;
     private int resultado;
+    private bool naoRodar;
 
     // Capturar Informações
-    public Calculadora(int _valorA, int _valorB, char _operacao)
+    public Calculadora(string[] _operacao)
     {
-        this.valorA = _valorA;
-        this.valorB = _valorB;
-        this.operacao = _operacao;
+        if (verificador(_operacao))
+        {
+            this.valorA = Convert.ToInt32(_operacao[0]);
+            this.valorB = Convert.ToInt32(_operacao[2]);
+            this.operacao = Convert.ToChar(_operacao[1]);
+
+            this.naoRodar = false;
+        }
+        else
+        {
+            this.naoRodar = true;
+
+            Console.WriteLine("Valores errados");
+            Thread.Sleep(2000);
+            Console.Clear();
+        }
+    }
+
+    // Verificação completa
+    private bool verificador(string[] _oper)
+    {
+        if (_oper.Count() > 3 || _oper.Count() < 3)
+        {
+            return false;
+        }
+        else if (verificarZero(_oper[2], _oper[1]))
+        {
+            Console.WriteLine("ERRO! Operação de divisão com 0!");
+            return false;
+        }
+        else if (verificarNumero(_oper[0]) && verificarNumero(_oper[2]) && verificarOperador(_oper[1]))
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    // Verificar número Inteiro
+    private bool verificarNumero(string _num)
+    {
+        try
+        {
+            int numero = Convert.ToInt32(_num);
+            return true;
+        }
+        catch
+        {
+            return false;
+        }
+    }
+
+    // Verificar Operador
+    private bool verificarOperador(string _operador)
+    {
+        try
+        {
+            char valorOperacao = Convert.ToChar(_operador);
+            
+            if ("+-/*".Contains(valorOperacao))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        catch
+        {
+            return false;
+        }
+    }
+
+    // Verificar Operacao com zero
+    private bool verificarZero(string numB, string operB)
+    {
+        int numero_zero = Convert.ToInt32(numB);
+        char char_zero = Convert.ToChar(operB);
+
+        if (numero_zero == 0 && "/".Contains(char_zero))
+        {
+            return true;
+        }
+        return false;
     }
 
     // Calculadora  
@@ -81,16 +168,8 @@ class Calculadora
                 definirResultado(valor_multiplicacao);
                 break;
             case "/":
-                if (valorB == 0)
-                {
-                    Console.WriteLine("Não é possível fazer divisão por 0.");
-                    break;
-                }
-                else
-                {
-                    int valor_divisao = valorA / valorB;
-                    definirResultado(valor_divisao);
-                }
+                int valor_divisao = valorA / valorB;
+                definirResultado(valor_divisao);
                 break;
             default:
                 break;
@@ -112,8 +191,13 @@ class Calculadora
     // Mostrar Resultado
     public void show()
     {
-        Thread.Sleep(2000);
-        Console.Clear();
-        Console.WriteLine($"Resultado da operação:\n{valorA} {operacao} {valorB} = {resultado}");
+        if (!naoRodar)
+        {
+            calcular();
+
+            Console.Clear();
+            Console.WriteLine($"Resultado da operação:\n{valorA} {operacao} {valorB} = {resultado}");
+            Thread.Sleep(3000);
+        }
     }
 }
